425ecf511d2eafde4e06f35bf3f72ddcffab21d6
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-03-01 11:58:35 +0900

    first commit

--- CarouselByUIScrollViewPractice/ViewController.swift
+++ CarouselByUIScrollViewPractice/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var scrollView: UIScrollView!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+
+		let size = CGSize(width: scrollView.frame.size.width, height: scrollView.frame.size.height)
+
+		let contentRect = CGRect(x: 0, y: 0, width: size.width * 4, height: size.height)
+		let contentView = UIView(frame: contentRect)
+
+		let subContentView = UIView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+		subContentView.backgroundColor = .green
+		contentView.addSubview(subContentView)
+
+		let secondContentView = UIView(frame: CGRect(x: size.width, y: 0, width: size.width, height: size.height))
+		secondContentView.backgroundColor = .blue
+		contentView.addSubview(secondContentView)
+
+		let thirdContentView = UIView(frame: CGRect(x: size.width * 2, y: 0, width: size.width, height: size.height))
+		thirdContentView.backgroundColor = .red
+		contentView.addSubview(thirdContentView)
+
+		let fourthContentView = UIView(frame: CGRect(x: size.width * 3, y: 0, width: size.width, height: size.height))
+		fourthContentView.backgroundColor = .yellow
+		contentView.addSubview(fourthContentView)
+
+		scrollView.addSubview(contentView)
+		scrollView.contentSize = contentView.frame.size
+		scrollView.contentOffset = CGPoint(x: size.width, y: 0)
 	}
 
 	override func didReceiveMemoryWarning() {
